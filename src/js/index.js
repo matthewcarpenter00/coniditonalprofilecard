c10810c814ede72906c84eb71bce0719bf5c1bd0
matthewcarpenter00 <matthew@firstlight.media>
2021-09-14 00:49:28 +0000

    conditional card

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,8 @@ import "../style/index.scss";
  * 
     {
         includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        background: null, // this is the url of the image that will used as background for the profile cover
+        avatarURL: null, // this is the url for the profile avatar
         socialMediaPosition: "left", // social media bar position (left or right)
         
         twitter: null, // social media usernames
@@ -33,15 +33,16 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://www.twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
+          
         </div>
     `;
 }
@@ -61,26 +62,35 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    github: "@username",
+    linkedin: "@username",
+    instagram: "@username",
+    name: "first name",
+    lastname: "last name",
+    role: "Job Title",
+    country: "Country",
+    city: "City"
   };
   render(window.variables); //render the card for the first time
 
+  // go through every picker element and run a loop (for each)
+
   document.querySelectorAll(".picker").forEach(function(elm) {
+    // add event listener when there is a change to the element
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
+      // create variable and assign it to the value of the "for" attribute
       const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+
+      // create an empty obejct for your values
       let values = {};
+      // create an array to fill the attribute values
       values[attribute] =
+        // this is like an if else statement
+        // if
         this.value == "" || this.value == "null"
           ? null
-          : this.value == "true"
+          : // else if
+          this.value == "true"
           ? true
           : this.value == "false"
           ? false
